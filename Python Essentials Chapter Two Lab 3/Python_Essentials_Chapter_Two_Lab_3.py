751d835eeb7fe2b6311dbb60a99a52716c386428
anil thapa <thapa_an1@hotmail.com>
2021-09-20 11:23:25 +0100

    Chapter 2 Lab 4

--- Python Essentials Chapter Two Lab 3/Python_Essentials_Chapter_Two_Lab_3.py
+++ Python Essentials Chapter Two Lab 3/Python_Essentials_Chapter_Two_Lab_3.py
@@ -4,19 +4,19 @@
 
 print("Hello, Python!")
 print("Anil")
-
+print()
 # Chapter 2 Lab 2
 # Using sep and end keyword arguments to achieve the expected output
 
 print("Programming","Essentials","in", sep="***", end="...")
 print("Python")
-
+print()
 # Chapter 2 Lab 3 
 
 # Minimizing the number of print() function invocations by inserting the \n sequence into the strings
 
 print("    *\n   * *\n  *   *\n *     *\n***   ***\n  *   *\n  *   *\n  *****")
-
+print()
 
 # The original code for Single Arrow display
 
@@ -69,3 +69,10 @@ print("    *       *     " * 2)
 print("    *       *     " * 2)
 print("    *       *     " * 2)
 print("    *********     " * 2)
+print()
+
+# Chapter 2 Lab 4
+# Writing a one-line piece of code using the print() function as well as the newline and escape characters
+
+print("I'm\" \n\"\"learning\"\" \n\"\"\"Python\"\"\"")
+print()
