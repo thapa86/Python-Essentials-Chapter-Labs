8a95a9bb2a87727b9df32aa4b44505958eba5970
anil thapa <thapa_an1@hotmail.com>
2021-09-20 10:31:49 +0100

    Chapt 2 Labs

--- Python Essentials Chapter Two Lab 3/Python_Essentials_Chapter_Two_Lab_3.py
+++ Python Essentials Chapter Two Lab 3/Python_Essentials_Chapter_Two_Lab_3.py
@@ -1,3 +1,25 @@
+
+#Chapter 2 Lab 1
+# Using the print() function to print the line "Hello, Python!" and "Anil"
+
+print("Hello, Python!")
+print("Anil")
+
+# Chapter 2 Lab 2
+# Using sep and end keyword arguments to achieve the expected output
+
+print("Programming","Essentials","in", sep="***", end="...")
+print("Python")
+
+# Chapter 2 Lab 3 
+
+# Minimizing the number of print() function invocations by inserting the \n sequence into the strings
+
+print("    *\n   * *\n  *   *\n *     *\n***   ***\n  *   *\n  *   *\n  *****")
+
+
+# The original code for Single Arrow display
+
 print("Single Arrow")
 print("    *")
 print("   * *")
@@ -8,6 +30,9 @@ print("  *   *")
 print("  *   *")
 print("  *****")
 print()
+
+# Making the arrow twice as large but keeping the proportions
+
 print("Doubled Arrow")
 print("        *")
 print("       * *")
@@ -25,6 +50,9 @@ print("    *       *")
 print("    *       *")
 print("    *********")
 print()
+
+# Duplicating the doubled arrows side by side by using "string" * 2
+
 print("Duplicated Arrows")
 print("        *         " * 2)
 print("       * *        " * 2)
